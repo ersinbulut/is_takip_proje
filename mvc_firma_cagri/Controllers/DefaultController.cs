744523f337beb91984b21ae1ffa48e89167abd61
Ersin BULUT <Ersin BULUT@LENOVO-DEV>
2026-02-12 15:36:05 +0300

    56

---
+++ mvc_firma_cagri/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using mvc_firma_cagri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_firma_cagri.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        DbİsTakipEntities2 db= new DbİsTakipEntities2();
+        public ActionResult AktifCagrilar()
+        {
+            var degerler = db.TblCagrilar
+                             .Where(x => x.Durum.HasValue && x.Durum.Value)
+                             .ToList();
+
+            return View(degerler);
+        }
+    }
+}
